d784de068e248d87139b5128c889ff32aae3a351
Mihail Vorobyev <1viklover@gmail.com>
2025-01-05 21:57:10 +0500

    initial commit

---
+++ JsonHelper.Net.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace JsonHelper.Net.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
